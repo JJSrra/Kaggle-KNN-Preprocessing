561b44fc7ead07293d5ff00aaf127c6c62c57f52
Juanjo Sierra <jjsierra103@gmail.com>
2019-02-23 20:25:41 +0100

    Uploaded Submission 1

--- Submissions/submission-1/kaggle-1.R
+++ Submissions/submission-1/kaggle-1.R
@@ -1,5 +1,11 @@
 # Import libraries
 library(ggplot2)
+library(mice)
+library(caret)
+library(e1071)
+
+# Initiate random seed
+set.seed(101)
 
 # Data reading
 # Mark "Source on Save" checkbox in RStudio to make sure the following line works!
@@ -16,6 +22,9 @@ ggplot(train, aes(C)) + geom_histogram(fill="red", col="darkred")
 
 # Binary classification, with several more examples from 0 class.
 # We might want to perform some oversampling/undersampling operation.
+
+# Treat class column as factor
+train$C = as.factor(train$C)
 
 # Check for any missing values in train
 anyNA(train)
@@ -36,3 +45,55 @@ sum(na.cols)
 
 # Remove rows with NAs
 train = train[-which(na.rows == TRUE),]
+
+# Check for outliers
+# Put NA where there is an outlier
+train[,1:50] = as.data.frame(apply(train[,1:50], 2, function(column) {
+  q1 = quantile(column)[2]
+  q3 = quantile(column)[4]
+  IQR.column = q3-q1
+  upper.bound = q3 + 1.5*IQR.column
+  lower.bound = q1 - 1.5*IQR.column
+  column[column >= upper.bound | column <= lower.bound] = NA
+  column
+}))
+
+# Check again the new amount of NAs
+na.rows = apply(is.na(train), 1, function (x) Reduce('|', x))
+sum(na.rows)
+
+# There's a huge amount of outliers in at least one attribute
+
+# Impute the "new missing values" (outliers) using the "mice" library
+# and the "Predictive mean matching" method
+impute = mice(train, m=3, maxit=3, method="pmm", seed=101)
+
+# Check the correlations between instances of the imputation
+cor(impute$imp$X1$`1`,impute$imp$X1$`3`)
+cor(impute$imp$X1$`2`,impute$imp$X1$`3`)
+cor(impute$imp$X1$`1`,impute$imp$X1$`2`)
+
+# Note that imputation 1 has more similarities with 2 and 3 than those two have with each other
+
+# Use imputation 1 for completing the dataset
+train = complete(impute, 1)
+
+# Check there are no missing values/outliers anymore
+anyNA(train)
+
+# Train KNN models with odd Ks from 11 to 15 Ks to detect the best configuration
+control = trainControl(method="cv", number=5)
+knn.model = train(train[,1:50], train$C, method="knn",
+                 metric="Accuracy", trControl=control,
+                 tuneLength=10)
+
+# Get the 3 results in case we detect overfitting in any of the models
+knn.model
+
+# Predict test labels
+predictions = predict(knn.model, test)
+predictions = ifelse(predictions == 1, 1, 0)
+
+# Export the predictions to a .csv file
+output = as.data.frame(cbind("Id"=1:length(predictions), "Prediction"=predictions))
+write.table(output, file="submission-1.csv", quote=FALSE, sep=",", row.names=FALSE)
